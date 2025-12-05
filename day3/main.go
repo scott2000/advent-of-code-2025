597330deb9d5e85ac95163baab7a645636818d45
Scott Taylor <scott11x8@gmail.com>
2025-12-05 14:22:07 -0600

    Day 3 part 2

--- day3/main.go
+++ day3/main.go
@@ -12,28 +12,64 @@ func main() {
 		panic(err)
 	}
 
-	total := 0
+	totalWithTwo := 0
+	totalWithTwelve := 0
 	for line := range strings.Lines(string(contents)) {
 		line = strings.TrimSpace(line)
-		total += findJoltage(line)
+		totalWithTwo += findJoltage(line, 2)
+		totalWithTwelve += findJoltage(line, 12)
 	}
-	fmt.Println(total)
+	fmt.Println(totalWithTwo)
+	fmt.Println(totalWithTwelve)
 }
 
-func findJoltage(line string) int {
-	highestIndex := 0
-	highestValue := '0'
-	for i, ch := range line[:len(line)-1] {
-		if ch > highestValue {
-			highestIndex, highestValue = i, ch
+func findJoltage(line string, numDigits int) int {
+	firstIndexOfEachDigit := make([]int, 10)
+	for i := range firstIndexOfEachDigit {
+		firstIndexOfEachDigit[i] = -1
+	}
+	for i, ch := range line {
+		digit := int(ch - '0')
+		if firstIndexOfEachDigit[digit] == -1 {
+			firstIndexOfEachDigit[digit] = i
 		}
 	}
 
-	secondHighestValue := '0'
-	for _, ch := range line[highestIndex+1:] {
-		if ch > secondHighestValue {
-			secondHighestValue = ch
+	findIndexInRange := func(digit int, startIndex int) int {
+		cachedIndex := firstIndexOfEachDigit[digit]
+		if cachedIndex == -1 || cachedIndex >= startIndex {
+			return cachedIndex
 		}
+
+		for i := startIndex; i < len(line); i++ {
+			newDigit := int(line[i] - '0')
+			if newDigit == digit {
+				firstIndexOfEachDigit[digit] = i
+				return i
+			}
+		}
+		firstIndexOfEachDigit[digit] = -1
+		return -1
 	}
-	return int(highestValue-'0')*10 + int(secondHighestValue-'0')
+
+	accumulator := 0
+	startIndex := 0
+outer:
+	for i := range numDigits {
+		for digit := 9; digit >= 0; digit-- {
+			index := findIndexInRange(digit, startIndex)
+			if index == -1 || index >= len(line)+i+1-numDigits {
+				continue
+			}
+
+			accumulator *= 10
+			accumulator += digit
+			startIndex = index + 1
+
+			continue outer
+		}
+		panic("No digits found")
+	}
+
+	return accumulator
 }
