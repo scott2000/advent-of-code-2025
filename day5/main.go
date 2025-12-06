63ddb0371639f7b9f17f3225d3d05cdfedfae824
Scott Taylor <scott11x8@gmail.com>
2025-12-05 18:45:34 -0600

    Day 5 part 1

---
+++ day5/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"cmp"
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	contents, err := os.ReadFile("inputs/day5.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	splitContents := strings.Split(string(contents), "\n\n")
+	rangesStr := splitContents[0]
+	queriesStr := splitContents[1]
+
+	var operations []Operation
+	for line := range strings.Lines(rangesStr) {
+		line = strings.TrimSpace(line)
+		splitLine := strings.Split(line, "-")
+		start, err := strconv.Atoi(splitLine[0])
+		if err != nil {
+			panic(err)
+		}
+		end, err := strconv.Atoi(splitLine[1])
+		if err != nil {
+			panic(err)
+		}
+		operations = append(operations,
+			Operation{position: start, isEnd: false},
+			Operation{position: end + 1, isEnd: true})
+	}
+
+	slices.SortFunc(operations, func(a, b Operation) int {
+		result := cmp.Compare(a.position, b.position)
+		if result != 0 {
+			return result
+		}
+		if a.isEnd == b.isEnd {
+			return 0
+		}
+		if a.isEnd {
+			return 1
+		}
+		return -1
+	})
+
+	var flattened []Operation
+	depth := 0
+	for _, operation := range operations {
+		if !operation.isEnd {
+			if depth == 0 {
+				flattened = append(flattened,
+					Operation{position: operation.position, isEnd: false})
+			}
+			depth++
+			continue
+		}
+
+		depth--
+		if depth == 0 {
+			flattened = append(flattened,
+				Operation{position: operation.position, isEnd: true})
+		}
+	}
+
+	freshCount := 0
+	for queryStr := range strings.Lines(queriesStr) {
+		queryStr = strings.TrimSpace(queryStr)
+		query, err := strconv.Atoi(queryStr)
+		if err != nil {
+			panic(err)
+		}
+
+		index, found := slices.BinarySearchFunc(flattened, query, func(operation Operation, query int) int {
+			return cmp.Compare(operation.position, query)
+		})
+		if found {
+			if !flattened[index].isEnd {
+				freshCount++
+			}
+		} else {
+			if index > 0 && !flattened[index-1].isEnd {
+				freshCount++
+			}
+		}
+	}
+
+	fmt.Println(freshCount)
+}
+
+type Operation struct {
+	position int
+	isEnd    bool
+}
